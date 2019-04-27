3723a721e5f55ef822bec95cbf2da19ef6bdc2f9
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2019-04-27 12:53:50 +0200

    Initial commit

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    private bool _serverThreadRunning = false;
+    private Thread _serverThread = null;
+    private Server _server = null;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (!_serverThreadRunning)
+            {
+                StartServer();
+            }
+            else
+            {
+                StopServer();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_serverThreadRunning)
+        {
+            if (_server.isFinished())
+            {
+                _serverThreadRunning = false;
+            }
+        }
+    }
+
+    private void StartServer()
+    {
+        Debug.Log("[SERVER] Starting server.");
+        _serverThreadRunning = true;
+        _server = new Server();
+        ThreadStart threadStart = new ThreadStart(_server.Process);
+        _serverThread = new Thread(threadStart);
+        _serverThread.Start();
+    }
+
+    private void StopServer()
+    {
+        Debug.Log("[SERVER] Stopping server.");
+        _server.PleaseStop();
+    }
+}
+
---
+++ Assets/Server.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Server
+{
+    private bool _needsToStop = false;
+    private bool _isFinished = false;
+    public void Process()
+    {
+        while (true)
+        {
+            if (_needsToStop)
+            {
+                Debug.Log("[SERVERTHREAD] Is stopping.");
+                break;
+            }
+            Debug.Log("[SERVERTHREAD] Processing...");
+            Thread.Sleep(500);
+        }
+        _isFinished = true;
+    }
+
+    public void PleaseStop()
+    {
+        _needsToStop = true;
+    }
+    public bool isFinished()
+    {
+        return _isFinished;
+    }
+}
